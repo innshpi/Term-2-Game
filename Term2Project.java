7bd5fbad0b7f8353a3d0d8dfd50cd5a37462366c
innshpi <innshpi@whs.school.nz>
2022-05-24 17:16:46 +1200

    all player 1 ships working

--- Term2Project.java
+++ Term2Project.java
@@ -10,6 +10,8 @@ public class Term2Project
     Scanner kb = new Scanner (System.in);
     
     boolean shipCtrl = true;
+    boolean player1Play = true;
+    boolean player2Play = false;
     
     private int ROWS = 10;
     private int COLS = 10;
@@ -34,17 +36,17 @@ public class Term2Project
     int[] shipPosX = {0, 0, 0, 0, 0};
     int[] shipPosY = {0, 1, 2, 3, 4};
     
-    int[] ship1FinalX = new int[5];
-    int[] ship2FinalX = new int[5];
-    int[] ship3FinalX = new int[5];
-    int[] ship4FinalX = new int[5];
-    int[] ship5FinalX = new int[5];
+    int[] ship1P1FinalX = new int[5];
+    int[] ship2P1FinalX = new int[5];
+    int[] ship3P1FinalX = new int[5];
+    int[] ship4P1FinalX = new int[5];
+    int[] ship5P1FinalX = new int[5];
     
-    int[] ship1FinalY = new int[5];
-    int[] ship2FinalY = new int[5];
-    int[] ship3FinalY = new int[5];
-    int[] ship4FinalY = new int[5];
-    int[] ship5FinalY = new int[5];
+    int[] ship1P1FinalY = new int[5];
+    int[] ship2P1FinalY = new int[5];
+    int[] ship3P1FinalY = new int[5];
+    int[] ship4P1FinalY = new int[5];
+    int[] ship5P1FinalY = new int[5];
     
     int[] shipLengthArray = {2, 3, 3, 4, 5};
     int shipLengthAdd = 0;
@@ -76,12 +78,11 @@ public class Term2Project
             for(int j=0;j<ROWS;j++){
                 setupBoardPlayer1[i][j] = wave;
                 for(int p=0;p<shipLength;p++)setupBoardPlayer1[shipPosY[p]][shipPosX[p]] = ship;
-                if (ship1Print)for(int k=0;k<2;k++)setupBoardPlayer1[ship1FinalY[k]][ship1FinalX[k]] = ship1Final;
-                
-                if (ship2Print)for(int l=0;l<3;l++)setupBoardPlayer1[ship2FinalX[l]][ship2FinalY[l]] = ship2Final;
-                if (ship3Print)for(int m=0;m<3;m++)setupBoardPlayer1[ship3FinalX[m]][ship3FinalY[m]] = ship3Final;
-                if (ship4Print)for(int n=0;n<4;n++)setupBoardPlayer1[ship4FinalX[n]][ship4FinalY[n]] = ship4Final;
-                if (ship5Print)for(int o=0;o<5;o++)setupBoardPlayer1[ship5FinalX[o]][ship5FinalY[o]] = ship5Final;
+                if (ship1Print)for(int k=0;k<2;k++)setupBoardPlayer1[ship1P1FinalY[k]][ship1P1FinalX[k]] = ship1Final;
+                if (ship2Print)for(int l=0;l<3;l++)setupBoardPlayer1[ship2P1FinalY[l]][ship2P1FinalX[l]] = ship2Final;
+                if (ship3Print)for(int m=0;m<3;m++)setupBoardPlayer1[ship3P1FinalY[m]][ship3P1FinalX[m]] = ship3Final;
+                if (ship4Print)for(int n=0;n<4;n++)setupBoardPlayer1[ship4P1FinalY[n]][ship4P1FinalX[n]] = ship4Final;
+                if (ship5Print)for(int o=0;o<5;o++)setupBoardPlayer1[ship5P1FinalY[o]][ship5P1FinalX[o]] = ship5Final;
                 System.out.print(setupBoardPlayer1[i][j]);
             }
             System.out.print(setupBoardChar + "\u3002");
@@ -156,28 +157,33 @@ public class Term2Project
     } 
     
     public void shipPrint(){
-        if(shipLengthAdd==0){
+        if(shipLengthAdd==0 && player1Play){
             ship1Print = true;
-            for(int i=0;i<2;i++)ship1FinalX[i] = shipPosX[i];
-            for(int i=0;i<2;i++)ship1FinalY[i] = shipPosY[i];
+            for(int i=0;i<2;i++)ship1P1FinalX[i] = shipPosX[i];
+            for(int i=0;i<2;i++)ship1P1FinalY[i] = shipPosY[i];
         }
-        
-        if(shipLengthAdd==1){
-            ship2Print = true; 
-            ship2FinalX[2] = shipPosX[2]; 
-            ship1FinalY[2] = shipPosY[2];
+        if(shipLengthAdd==1 && player1Play){
+            ship2Print = true;
+            for(int i=0;i<3;i++)ship2P1FinalX[i] = shipPosX[i];
+            for(int i=0;i<3;i++)ship2P1FinalY[i] = shipPosY[i];
         }
-        if(shipLengthAdd==2){
+        if(shipLengthAdd==2 && player1Play){
             ship3Print = true; 
-            ship3FinalX[3] = shipPosX[3]; 
-            ship1FinalY[3] = shipPosY[3];
+            for(int i=0;i<3;i++)ship3P1FinalX[i] = shipPosX[i];
+            for(int i=0;i<3;i++)ship3P1FinalY[i] = shipPosY[i];
         }
-        // if(shipLengthAdd==3){ship4Print = true; ship4FinalX[4] = shipPosX[4]; ship1FinalY[4] = shipPosY[4];}
-        // if(shipLengthAdd==4){ship5Print = true; ship5FinalX[5] = shipPosX[5]; ship1FinalY[5] = shipPosY[5];}
-        
+        if(shipLengthAdd==3 && player1Play){
+            ship4Print = true; 
+            for(int i=0;i<4;i++)ship4P1FinalX[i] = shipPosX[i];
+            for(int i=0;i<4;i++)ship4P1FinalY[i] = shipPosY[i];
+        }
+        if(shipLengthAdd==4 && player1Play){
+            ship5Print = true;
+            for(int i=0;i<5;i++)ship5P1FinalX[i] = shipPosX[i];
+            for(int i=0;i<5;i++)ship5P1FinalY[i] = shipPosY[i];
+        }
         for(int i=0;i<5;i++)shipPosX[i] = 0;
         for(int i=0;i<5;i++)shipPosY[i] = i;
-        
         shipLengthAdd++;
         shipLength = shipLengthArray[shipLengthAdd];
     }
